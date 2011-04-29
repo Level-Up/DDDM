cd7f11ad438c77af8948b8abf64c4f55c49d4617
Aaron Bassett <me@aaronbassett.com>
2011-04-29 20:36:29 +0100

    Start user profiles display and registration

--- dddm/account/models.py
+++ dddm/account/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django.utils.hashcompat import md5_constructor
 from django.contrib.auth.models import User
+import urllib
 
 
 class UserProfile(models.Model):
@@ -17,5 +19,52 @@ class UserProfile(models.Model):
     forrst = models.CharField(max_length=50, blank=True)
     github = models.CharField(max_length=50, blank=True)
     delicious = models.CharField(max_length=50, blank=True)
+    
+    
+    def get_absolute_url(self):
+        return "/user/%d" % self.user.pk
+    
+    def get_gravatar(self, size=80, default='wavatar'):
+        """ Get's a Gravatar for a email address.
+        From: https://github.com/bread-and-pepper/django-userena
+    
+        :param size:
+            The size in pixels of one side of the Gravatar's square image.
+            Optional, if not supplied will default to ``80``.
+    
+        :param default:
+            Defines what should be displayed if no image is found for this user.
+            Optional argument which defaults to ``identicon``. The argument can be
+            a URI to an image or one of the following options:
+    
+                ``404``
+                    Do not load any image if none is associated with the email
+                    hash, instead return an HTTP 404 (File Not Found) response.
+    
+                ``mm``
+                    Mystery-man, a simple, cartoon-style silhouetted outline of a
+                    person (does not vary by email hash).
+    
+                ``identicon``
+                    A geometric pattern based on an email hash.
+    
+                ``monsterid``
+                    A generated 'monster' with different colors, faces, etc.
+    
+                ``wavatar``
+                    Generated faces with differing features and backgrounds
+    
+        :return: The URI pointing to the Gravatar.
+    
+        """
+        base_url = 'http://www.gravatar.com/avatar/'
+    
+        gravatar_url = '%(base_url)s%(gravatar_id)s?' % \
+                {'base_url': base_url,
+                 'gravatar_id': md5_constructor(self.user.email.lower()).hexdigest()}
+    
+        gravatar_url += urllib.urlencode({'s': str(size),
+                                          'd': default})
+        return gravatar_url
 
 User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
